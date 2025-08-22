61b6d7daeab2715235e75f6127ac64f03a4d66f3
cr <cbalanta@playtechla.com>
2025-08-22 17:13:45 -0500

    cambio
    Se agrego @jsonIgnore para poder ignora la lista de productos para que no se serialice

--- proyectoVenta/src/main/java/com/example/proyectoVenta/categoria/Entity/Categoria.java
+++ proyectoVenta/src/main/java/com/example/proyectoVenta/categoria/Entity/Categoria.java
@@ -1,6 +1,7 @@
 package com.example.proyectoVenta.categoria.Entity;
 
 import com.example.proyectoVenta.productos.Entity.Productos;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -19,6 +20,7 @@ public class Categoria {
     String nombreCategoria;
 
     @ManyToMany(mappedBy = "categorias")
+    @JsonIgnore
     private Set<Productos> productos = new HashSet<>();
 
     public Categoria() {
